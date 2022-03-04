2e4bce7fd7aa1c83b0f58628c8b8d51663b8f66f
吴乙卫 <idwuyiwei@foxmail.com>
2022-03-04 13:56:32 +0800

    add log4js

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 const { models } = require('./sequelize');
+const log4js = require("log4js");
+
+log4js.configure({
+    appenders: {
+        console: { type: 'console' },
+        multi: {
+            type: 'multiFile',
+            base: 'logs/',
+            property: 'level',
+            extension: '.log',
+            overwrite: false,
+            maxLogSize: 104857600,
+            backups: 1000,
+            compress: true
+        }
+    },
+    categories: { default: { appenders: ['multi', 'console'], level: 'debug' } }
+});
+const logger = log4js.getLogger();
 
 (async () => {
     const taskid = await models.movedata.create({
@@ -8,6 +27,6 @@ const { models } = require('./sequelize');
             state: 'running'
         })
         .then(result => { return result.id })
-        .catch(err => { console.log(err) })
-    console.log("插入数据 id 为:" + taskid);
+        .catch(err => { logger.error(err) })
+    logger.info("插入数据 id 为:" + taskid);
 })()
